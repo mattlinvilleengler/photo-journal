27cb790382f9291b5c9f7f0fe46507284c1ef364
Matt Linville-Engler <TheFreshPrince@Matts-MacBook-Pro.local>
2016-12-05 00:56:32 -0800

    updates

--- src/app/services/photo-journal-service.service.ts
+++ src/app/services/photo-journal-service.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 declare var firebase: any;
 
 @Injectable()
 export class PhotoJournalService {
-  database = firebase.database();
-
-  constructor() { }
-
+  database:any;
+  public firebase: any = firebase;
+  private config: {} = {
+    apiKey: "AIzaSyCHlLQG2iPPGsfgqBvgmQpbLtADzWTyTvY",
+    authDomain: "photojournal-3894c.firebaseapp.com",
+    databaseURL: "https://photojournal-3894c.firebaseio.com",
+    storageBucket: "photojournal-3894c.appspot.com",
+    messagingSenderId: "87741841068"
+  };
+  constructor() { 
+      firebase.initializeApp(this.config);
+      this.database = firebase.database();
+  }
+  public getDB(){
+    return this.database;
+  }
   public sortPhotosAscending(arr) {
     arr.sort(function (a, b) { return b.rating - a.rating });
     return arr;
@@ -31,7 +43,41 @@ export class PhotoJournalService {
     return arr;
   }
   public updatePhoto(data) {
-    firebase.database().ref('photos/' + data.UID).set(data);
+    this.database.ref('photos/' + data.UID).set(data);
+  }
+  photoObjectToArray(photos) {
+    var photoArray = [];
+    for (var key in photos) {
+      photos[key].UID = key;
+      photoArray.push(photos[key]);
+    }
+    photoArray = this.sortPhotosAscending(photoArray);
+    return photoArray;
+  }
+  sortPhotos(PhotoArr, show, unRatedOnly, reverse) {
+    var sortedPhotos = this.subSortLists(PhotoArr, show, unRatedOnly, reverse);
+    sortedPhotos.forEach(arr => { arr = this.subSortPhotos(arr) });
+    return sortedPhotos;
+  }
+  subSortLists(arr, show, unRatedOnly, reverse) {
+    var sorted = [[], [], [], [], [], []];
+    arr.forEach(p => {
+      if (show) {
+        sorted[p.rating].push(p);
+      } else if (unRatedOnly) {
+        if (p.rating < 1) {
+          sorted[p.rating].push(p);
+        }
+      } else if (!unRatedOnly) {
+        if (p.rating > 1) {
+          sorted[p.rating].push(p);
+        }
+      }
+    });
+    if (!reverse) {
+      sorted.reverse();
+    }
+    return sorted;
   }
 
 }
